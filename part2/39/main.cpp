664af58025f68cc37d3f399f0a87fe9fab3f6900
Mass <mazengrong1211@163.com>
2020-05-28 10:18:12 +0800

    add p2/40

--- part2/39/main.cpp
+++ part2/39/main.cpp
@@ -4,34 +4,80 @@ using namespace std;
 
 bool fibon_ele(int, int&);
 
+bool print_sequence(int pos);
+
 int main(void)
 {
-    int pos;
-    cout << "Please enter a position" << endl;
-    cin >> pos;
+	int pos;
 
-    int elem;
-    if( fibon_ele(pos, elem))
-        cout << "element # " << pos
-            << " is " << elem << endl;
-    else cout << "Sorry. Could not calculate element # "
-            << pos << endl;
+
+	while(true){
+		cout << "Please enter a position" << endl;
+		if(!(cin >> pos))
+			break;
+
+		int elem;
+		if( fibon_ele(pos, elem)){
+			cout << "element # " << pos
+				<< " is " << elem << endl;
+			print_sequence(pos);
+		}
+		else cout << "Sorry. Could not calculate element # "
+			<< pos << endl;
+	}
+
 }
 
 
 bool fibon_ele(int pos, int& elem)
 {
-    // check pos
-    if ( pos <= 0 or pos > 1024)
-        {elem = 0; return false;}
-    
-    elem = 1;
-    int n_2 = 1, n_1 = 1;
+	// check pos
+	if ( pos <= 0 or pos > 1024)
+	{elem = 0; return false;}
 
-    for(int ix = 3; ix <= pos; ++ix){
-        elem = n_2 + n_1;
-        n_2 = n_1; n_1 = elem;
-    }
+	elem = 1;
+	int n_2 = 1, n_1 = 1;
 
-    return true;
+	for(int ix = 3; ix <= pos; ++ix){
+		elem = n_2 + n_1;
+		n_2 = n_1; n_1 = elem;
+	}
+
+	return true;
+}
+
+
+bool print_sequence(int pos)
+{
+	if( pos <= 0 || pos >= 1024){
+		cerr << "invalid position: " << pos	
+			<<	" -- cannot handle request!\n";
+
+		return false;
+	}
+
+	cout << "The Fibonacci Sequence for "
+		<< pos << "positions: \n\t";
+
+
+	switch(pos){
+		default:
+		case 2:
+			cout << "1 ";
+		case 1:
+			cout << "1 ";
+			break;
+	}
+
+	int elem;
+	int n_2 = 1, n_1 = 1;
+	for (int ix = 3; ix <= pos; ++ix){
+		elem = n_2 + n_1;
+		n_2 = n_1; n_1 = elem ;
+
+		cout << elem << (!(ix % 10) ? "\n\t" : " " );
+	}
+	cout << endl;
+
+	return true;
 }
